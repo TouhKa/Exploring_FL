9b77e205f6dd6dc48762bc8a3b0162f7e856bb89
TouhKa <pia.s.schmidt@googlemail.com>
2021-05-19 14:39:55 +0200

    Write non iid dataset to csv

--- iid_dataset.py
+++ iid_dataset.py
@@ -6,6 +6,7 @@ DATA_DIR = "./data/"
 OUT_DIR = "./out/"
 
 file_app_usages = "App_usage_trace.txt"
+Non_IID_file_name = "top_90_apps.csv"
 IID_file_name = "top_90_apps_IID.csv"
 q = 0.9
 
@@ -31,17 +32,23 @@ def main():
   quantile = np.quantile(counted_apps.values, q)
   print(f"Quantile threshold: {quantile}")
   IID_used_apps = counted_apps.loc[counted_apps.values >= quantile, :]
-
-  min_usage = min(IID_used_apps)
   print(f"# Apps: {len(IID_used_apps)}")
-  print(f"Min Usage: {min_usage}")
 
   syntetic_ids = [i[0] for i in IID_used_apps.index]
   df_syntetic = df.loc[df["App_ID"].isin(syntetic_ids)].reset_index()
+  #write non_iid_dataset to csv
+  df_syntetic.to_csv(DATA_DIR+ Non_IID_file_name, encoding="utf8")
+
+  #create IID dataset without users
   df_syntetic = df_syntetic.drop(["User_ID", "index"], axis=1)
   print(f"Discarded rows: {len(df)- len(df_syntetic)}")
   print(f"Remaining Rows: {len(df_syntetic)}")
 
+  #every app has the same amount of entries
+  min_usage = min(IID_used_apps)
+  print(f"Min Usage: {min_usage}")
+
+  #get random ids per app except
   random_idx = []
   for id in syntetic_ids[:-1]:
     idx = df_syntetic.loc[df_syntetic["App_ID"] == id].sample(frac = 1)[:min_usage].index
