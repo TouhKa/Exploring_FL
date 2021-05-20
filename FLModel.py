a3aaa45185bcb1119bfc2ce3cebbcd039280fea9
TouhKa <pia.s.schmidt@googlemail.com>
2021-05-20 12:33:45 +0200

    Fixed some stuff

--- FLModel.py
+++ FLModel.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 class FLModel(tf.keras.Model):
   def __init__(self, number_of_classes):
     super(FLModel, self).__init__()
+    tf.keras.backend.set_floatx('float64')
     initializer = tf.keras.initializers.Zeros()
     self.dense1 = tf.keras.layers.Dense(500, activation=tf.nn.relu, kernel_initializer=initializer)
     self.dense2 = tf.keras.layers.Dense(number_of_classes, kernel_initializer=initializer, activation=tf.nn.softmax)
