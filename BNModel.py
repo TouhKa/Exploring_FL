1c3721b828417e116cf7b301b0c965c15a05fef8
TouhKa <pia.s.schmidt@googlemail.com>
2021-05-26 09:17:38 +0200

    Restructured model with batch normalization

--- BNModel.py
+++ BNModel.py
@@ -5,8 +5,10 @@ class FLModel(tf.keras.Model):
     tf.keras.backend.set_floatx('float32')
     initializer = tf.keras.initializers.Zeros()
     self.dense1 = tf.keras.layers.Dense(500, activation=tf.nn.relu, kernel_initializer=initializer)
+    self.bn1 = tf.keras.layers.BatchNormalization()
     self.dense2 = tf.keras.layers.Dense(number_of_classes, kernel_initializer=initializer, activation=tf.nn.softmax)
 
-  def call(self, inputs):
+  def call(self, inputs, training=False):
     x = self.dense1(inputs)
+    x = self.bn1(x, training=training)
     return self.dense2(x)
